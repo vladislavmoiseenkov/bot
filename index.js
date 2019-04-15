d6dd1d6fdc33b292100cd6eef829d48d75523d3a
Vlad <vladislavmoiseenkov@gmail.com>
2019-04-15 10:01:00 +0300

    ! fix: lint

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const bodyParser = require('body-parser');
 const env = require('node-env-file');
 const axios = require('axios');
 
+// eslint-disable-next-line
 env(__dirname + '/.env');
 
 const FACEBOOK_API_URL = 'https://graph.facebook.com/v2.6/me';
-const PAGE_TOKEN = process.env.PAGE_TOKEN;
-const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-const PORT = process.env.PORT;
+const { PAGE_TOKEN } = process.env;
+const { VERIFY_TOKEN } = process.env;
+const { PORT } = process.env;
+
+async function sendMessage(recipientId, msgText, quickRepliesBtns = null) {
+  const data = {
+    recipient: {
+      id: recipientId,
+    },
+    message: {
+      text: msgText,
+    },
+  };
+
+  if (quickRepliesBtns) {
+    data.message.quick_replies = quickRepliesBtns;
+  }
+
+  return axios.post(`${FACEBOOK_API_URL}/messages?access_token=${PAGE_TOKEN}`, data);
+}
 
 const MAIN_MENU_BTN = [
   {
@@ -41,22 +59,18 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/webhook', (req, res) => {
-
   // Разбор параметров запроса
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
 
   // Проверка, имеются ли в запросе mode и token
   if (mode && token) {
-
     // Проверка правильности mode и token
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
       // Отправка токена challenge из запроса
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-
     } else {
       // Отправка ответа '403 Forbidden' если верифицировать токен не удалось
       res.sendStatus(403);
@@ -65,66 +79,45 @@ app.get('/webhook', (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-
-  let { body }= req;
+  const { body } = req;
 
   if (body.object !== 'page') {
     res.sendStatus(404);
     return;
   }
 
+  // eslint-disable-next-line
   body.entry.forEach(async (entry) => {
+    const webhookEvent = entry.messaging[0];
+    const senderPsid = webhookEvent.sender.id;
 
-    let webhook_event = entry.messaging[0];
-    let sender_psid = webhook_event.sender.id;
-
-    if (webhook_event.message) {
-      if (webhook_event.message.quick_reply) {
-        switch (webhook_event.message.quick_reply.payload) {
+    if (webhookEvent.message) {
+      if (webhookEvent.message.quick_reply) {
+        switch (webhookEvent.message.quick_reply.payload) {
           case 'OPEN_MAIN_MENU':
-            await sendMessage(sender_psid, 'Hello, I\'m your main menu!');
+            await sendMessage(senderPsid, 'Hello, I\'m your main menu!');
             return res.status(200).send('Click to "Open main menu button"');
+          default:
+            await sendMessage(senderPsid, 'Hello, I\'m your main menu!');
+            return res.status(200).send('');
         }
       }
-      return res.status(200).send('Message: ' + webhook_event.message.text);
+      return res.status(200).send(`Message: ${webhookEvent.message.text}`);
     }
 
-    if (webhook_event.postback) {
-
-      switch (webhook_event.postback.payload) {
+    if (webhookEvent.postback) {
+      switch (webhookEvent.postback.payload) {
         case 'GET_STARTED':
-          await sendMessage(sender_psid, 'Main menu', MAIN_MENU_BTN);
+          await sendMessage(senderPsid, 'Main menu', MAIN_MENU_BTN);
           return res.status(200).send('Get start btn clicked');
         case 'MAIN_MENU_PAYLOAD':
-          await sendMessage(sender_psid, 'Main menu', MAIN_MENU_BTN);
+          await sendMessage(senderPsid, 'Main menu', MAIN_MENU_BTN);
           return res.status(200).send('Main menu opened');
         default:
           return res.status(200).send('Otherwise');
-
       }
     }
-
   });
-
 });
 
 app.listen(PORT, () => console.log(`Server start on port: ${PORT}`));
-
-
-
-async function sendMessage(recipientId, msgText, quickRepliesBtns = null) {
-  const data = {
-    recipient:{
-      id: recipientId
-    },
-    message: {
-      text: msgText
-    }
-  };
-
-  if (quickRepliesBtns) {
-    data.message.quick_replies = quickRepliesBtns;
-  }
-
-  return axios.post(`${FACEBOOK_API_URL}/messages?access_token=${PAGE_TOKEN}`, data);
-}
