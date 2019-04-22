ca22606c000820e466926de32b350ed75624f50e
Vlad <vladislavmoiseenkov@gmail.com>
2019-04-22 09:26:59 +0300

    + add: add purchases

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,15 +5,18 @@ const
     getProducts,
     getProductById,
     MAIN_MENU_BTN,
+    LOCATION,
+    PHONE,
   } = require('../helpers');
 const UserFavourites = require('../models/UserFavourites');
-// const UserPurchase = require('../models/UserPurchase');
+const UserPurchase = require('../models/UserPurchase');
 
 
 module.exports = {
   async index(req, res) {
     try {
       const { body } = req;
+      let phoneNumber;
 
       if (body.object !== 'page') {
         res.sendStatus(404);
@@ -26,6 +29,14 @@ module.exports = {
 
         if (webhookEvent.message) {
           if (webhookEvent.message.quick_reply) {
+            // eslint-disable-next-line
+            const phoneRe = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
+
+            if (phoneRe.test(webhookEvent.message.quick_reply.payload)) {
+              phoneNumber = webhookEvent.message.quick_reply.payload;
+              webhookEvent.message.quick_reply.payload = 'PHONE';
+            }
+
             switch (webhookEvent.message.quick_reply.payload) {
               case 'OPEN_MAIN_MENU':
                 await sendMessage(senderPsid, 'Hello, I\'m your main menu!');
@@ -65,11 +76,55 @@ module.exports = {
                     message: e,
                   });
                 }
+              case 'PHONE':
+                try {
+                  const lastUserPurchase = await UserPurchase.findOne(
+                    { userId: senderPsid },
+                  );
+
+                  // eslint-disable-next-line
+                  lastUserPurchase.products[lastUserPurchase.products.length - 1].phone = phoneNumber;
+                  await UserPurchase.findOneAndUpdate(
+                    { userId: senderPsid },
+                    { $set: { products: lastUserPurchase.products } },
+                  );
+
+                  // eslint-disable-next-line
+                  await sendMessage(senderPsid, 'Thank you! Please share you location for delivery', LOCATION);
+                  return res.status(200).send({
+                    phone: phoneNumber,
+                  });
+                } catch (e) {
+                  console.error(e);
+                  return res.status(500).send(e);
+                }
+              case 'OPEN_PURCHASES':
+                try {
+                  const userPurchase = await UserPurchase.findOne({ userId: senderPsid });
+
+                  // console.log([...purchase]);
+
+                  await sendList(senderPsid, userPurchase.products, false, false);
+
+                  return res.status(200).send({ message: 'Purchases' });
+                } catch (e) {
+                  console.error(e);
+                  return res.status(500).send(e);
+                }
               default:
                 await sendMessage(senderPsid, 'Under construction');
                 return res.status(200).send('');
             }
           }
+
+          if (webhookEvent.message.attachments) {
+            // eslint-disable-next-line
+            sendMessage(senderPsid, 'Our courier will contact you within 2 hours', null);
+            return res.status(200).send({
+              message: 'webhookEvent.message.attachment',
+            });
+          }
+
           return res.status(200).send(`Message: ${webhookEvent.message.text}`);
         }
 
@@ -99,11 +154,14 @@ module.exports = {
                 return res.status(500).send(e);
               }
             case 'ADD_TO_FAVOURITES':
+              // eslint-disable-next-line
               const productById = await getProductById(payload[1]);
 
+              // eslint-disable-next-line
               const userFavoritesModel = await UserFavourites.findOne(
-                { userId: senderPsid },
-                err => console.error(err),
+                { userId: senderPsid }, (err) => {
+                  if (err) return console.log(err);
+                },
               );
 
               if (!userFavoritesModel) {
@@ -111,16 +169,18 @@ module.exports = {
                   {
                     userId: senderPsid,
                     products: [...productById],
+                  }, (err) => {
+                    if (err) return console.log(err);
                   },
-                  err => console.error(err),
                 );
+
+                await sendMessage(senderPsid, 'Added to favourites', MAIN_MENU_BTN);
 
                 return res.status(200).send({
                   message: 'Add to favourites',
                 });
               }
 
-              // console.log('userFavoritesModel.products', userFavoritesModel.products);
 
               // eslint-disable-next-line
               const product = userFavoritesModel.products.find(_product => +_product.sku === +payload[1]);
@@ -131,10 +191,62 @@ module.exports = {
                 );
               }
 
+              await sendMessage(senderPsid, 'Added to favourites', MAIN_MENU_BTN);
+
               return res.status(200).send({
                 message: 'Add to favourites',
               });
-              // break;
+            case 'VIEW_MORE':
+              try {
+                // eslint-disable-next-line
+                const viewProductById = await getProductById(payload[1]);
+
+                await sendList(senderPsid, [...viewProductById], false, true);
+
+                return res.status(200).send('View More');
+              } catch (e) {
+                console.error(e);
+                return res.status(500).send(e);
+              }
+            case 'BUY':
+              try {
+                // eslint-disable-next-line
+                const productById = await getProductById(payload[1]);
+
+                // eslint-disable-next-line
+                const userPurchaseModel = await UserPurchase.findOne(
+                  { userId: senderPsid },
+                  err => console.error(err),
+                );
+
+                if (!userPurchaseModel) {
+                  await UserPurchase.create(
+                    {
+                      userId: senderPsid,
+                      products: [...productById],
+                    },
+                    err => console.error(err),
+                  );
+                } else {
+                  try {
+                    await UserPurchase.updateOne(
+                      { userId: senderPsid },
+                      { $push: { products: productById } },
+                    );
+                  } catch (e) {
+                    console.error('e', e);
+                  }
+                }
+
+                await sendMessage(senderPsid, 'Share your phone number, please', PHONE);
+
+                return res.status(200).send({
+                  message: 'Bought',
+                });
+              } catch (e) {
+                console.error(e);
+                return res.status(500).send(e);
+              }
             default:
               return res.status(200).send('Otherwise');
           }
